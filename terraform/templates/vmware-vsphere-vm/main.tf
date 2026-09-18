c91e425ca048e5ebb9910aad591b83b2d7cc25bd
chmajster <44300091+chmajster@users.noreply.github.com>
2026-09-18 05:54:22 +0200

    feat: add vmware-vsphere-vm Terraform template

---
+++ terraform/templates/vmware-vsphere-vm/main.tf
@@ -0,0 +1,69 @@
+terraform {
+  required_version = ">= 1.8.0"
+  required_providers {
+    vsphere = {
+      source  = "vmware/vsphere"
+      version = ">= 2.10, < 3.0"
+    }
+  }
+}
+
+provider "vsphere" {}
+
+data "vsphere_datacenter" "dc" {
+  name = var.datacenter
+}
+
+data "vsphere_datastore" "datastore" {
+  name          = var.datastore
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+
+data "vsphere_compute_cluster" "cluster" {
+  name          = var.cluster
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+
+data "vsphere_network" "network" {
+  name          = var.network
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+
+data "vsphere_virtual_machine" "template" {
+  name          = var.template
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+
+resource "vsphere_virtual_machine" "vm" {
+  name             = var.name
+  folder           = var.folder
+  resource_pool_id = data.vsphere_compute_cluster.cluster.resource_pool_id
+  datastore_id     = data.vsphere_datastore.datastore.id
+  num_cpus         = var.cpu
+  memory           = var.memory
+  guest_id         = data.vsphere_virtual_machine.template.guest_id
+  scsi_type        = data.vsphere_virtual_machine.template.scsi_type
+
+  network_interface {
+    network_id   = data.vsphere_network.network.id
+    adapter_type = data.vsphere_virtual_machine.template.network_interface_types[0]
+  }
+
+  disk {
+    label            = "disk0"
+    size             = max(var.disk, data.vsphere_virtual_machine.template.disks[0].size)
+    thin_provisioned = data.vsphere_virtual_machine.template.disks[0].thin_provisioned
+  }
+
+  clone {
+    template_uuid = data.vsphere_virtual_machine.template.id
+  }
+}
+
+output "resource_id" {
+  value = vsphere_virtual_machine.vm.id
+}
+
+output "primary_ip" {
+  value = vsphere_virtual_machine.vm.default_ip_address
+}
